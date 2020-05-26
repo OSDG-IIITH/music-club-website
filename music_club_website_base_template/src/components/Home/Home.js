7340b282be83b567c574271c43b1667723e54129
rvk7895 <rkal2009@gmail.com>
2020-05-27 00:47:45 +0530

    Updated README.md and removed extra dependency imports from Home.js

--- music_club_website_base_template/src/components/Home/Home.js
+++ music_club_website_base_template/src/components/Home/Home.js
@@ -1,12 +1,4 @@
 import React,{Component} from 'react';
-import 'bootstrap/dist/css/bootstrap.css';
-//@ts-ignore
-import 'bootstrap/dist/js/bootstrap';
-// to import react-bootstrap import {...} from 'react-bootstrap'
-import 'jquery';
-import 'popper.js';
-
-
 
 class Home extends Component {
   render(){
