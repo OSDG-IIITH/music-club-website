3cec7d3187c8693af2bfbccb14fa1850803cfe80
rvk7895 <ritvik.kalra@research.iiit.ac.in>
2020-05-30 18:54:34 +0530

    bugs fixed in home.js and app.js

--- music_club_website_base_template/src/App.js
+++ music_club_website_base_template/src/App.js
@@ -1,7 +1,6 @@
 import React,{Component} from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-<<<<<<< HEAD
-=======
+
 // import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 //@ts-ignore
