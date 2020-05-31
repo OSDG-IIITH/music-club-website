94330c931d327ba67b463465b8bf0e7153b32c5a
rvk7895 <ritvik.kalra@research.iiit.ac.in>
2020-06-01 01:03:43 +0530

    made the page responsive

--- music_club_website_base_template/src/components/Timeline/Cards/Cards.js
+++ music_club_website_base_template/src/components/Timeline/Cards/Cards.js
@@ -1,17 +1,28 @@
 import React from 'react'
 import './Cards.css'
+import './FadeEffect'
 
 const Card = ({events}) => {
     const eventsList= events.map(event =>{
-        return(
-        <div className={`card-container container-fluid ${event.id%2?"left":"right"}`} key={event.id} id={`card${event.id}`}>
+        if(event.id < 10){
+            return(
+            <div className={`card-container container-fluid ${event.id%2?"left":"right"}`} key={event.id} id={`card${event.id}`}>
+                <div className="content">
+                    <h2>{event.title}</h2>
+                    <p>{event.link}</p>
+                </div>
+            </div>
+            )
+        }
+    })
+    eventsList.push(
+        <div className='card-container container-fluid right' key='10' id='card10'>
             <div className="content">
-                <h2>{event.title}</h2>
-                <p>{event.link}</p>
+                <h2>Archives</h2>
+                <p>alskdjask</p>
             </div>
         </div>
-        )
-    })
+    )
     return (    
         <div className="container-fluid cards">
             {eventsList}
