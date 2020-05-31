175e99202a61641e2c67ba94ee2f7132223611e3
Mehul1604 <mathurmehul3@gmail.com>
2020-05-31 17:29:43 +0530

    Carousel fixedd

--- music_club_website_base_template/src/components/Home/Home.js
+++ music_club_website_base_template/src/components/Home/Home.js
@@ -28,11 +28,25 @@ class Home extends Component {
      this.eventDesc1.current.style.transform = 'translate(-1100px , 0px)';
     }
 
-    else if(this.eventTitle2.current){
+    if(this.eventTitle2.current){
       this.eventTitle2.current.style.opacity='20%' ;
      this.eventDesc2.current.style.opacity='20%';
      this.eventTitle2.current.style.transform = 'translate(0px , -700px)';
      this.eventDesc2.current.style.transform = 'translate(-1100px , 0px)';
+    }
+
+    if(this.eventTitle3.current){
+      this.eventTitle3.current.style.opacity='20%' ;
+     this.eventDesc3.current.style.opacity='20%';
+     this.eventTitle3.current.style.transform = 'translate(0px , -700px)';
+     this.eventDesc3.current.style.transform = 'translate(-1100px , 0px)';
+    }
+
+    if(this.eventTitle4.current){
+      this.eventTitle4.current.style.opacity='20%' ;
+     this.eventDesc4.current.style.opacity='20%';
+     this.eventTitle4.current.style.transform = 'translate(0px , -700px)';
+     this.eventDesc4.current.style.transform = 'translate(-1100px , 0px)';
     }
       
     
@@ -48,14 +62,31 @@ class Home extends Component {
      this.eventDesc1.current.style.opacity='100%';
      this.eventTitle1.current.style.transform = 'translate(0px , 0px)';
      this.eventDesc1.current.style.transform = 'translate(0px , 0px)';
+     
     }
 
-    else if(this.eventTitle2.current){
+    if(this.eventTitle2.current){
       this.eventTitle2.current.style.opacity='100%' ;
      this.eventDesc2.current.style.opacity='100%';
      this.eventTitle2.current.style.transform = 'translate(0px , 0px)';
      this.eventDesc2.current.style.transform = 'translate(0px , 0px)';
     }
+
+    if(this.eventTitle3.current){
+      this.eventTitle3.current.style.opacity='100%' ;
+     this.eventDesc3.current.style.opacity='100%';
+     this.eventTitle3.current.style.transform = 'translate(0px , 0px)';
+     this.eventDesc3.current.style.transform = 'translate(0px , 0px)';
+     
+    }
+
+    if(this.eventTitle4.current){
+      this.eventTitle4.current.style.opacity='100%' ;
+     this.eventDesc4.current.style.opacity='100%';
+     this.eventTitle4.current.style.transform = 'translate(0px , 0px)';
+     this.eventDesc4.current.style.transform = 'translate(0px , 0px)';
+    }
+
    }
 
    eventTitle1  = React.createRef();
@@ -72,30 +103,30 @@ class Home extends Component {
     return (
       <React.Fragment>
       <div className="container-fluid mainDiv bg-dark">
-        <AwesomeSlider fillParent={false} className="carousel container-sm" cssModule={styles} transitionDelay={500} mobileTouch={true} bullets={true} onTransitionRequest={this.fade} onTransitionEnd={this.bringBack}  >
+        <AwesomeSlider fillParent={false} className="carousel container-sm" cssModule={styles} transitionDelay={500} mobileTouch={true} bullets={true} onTransitionStart={this.fade} onTransitionEnd={this.bringBack}  >
           <div className=" container-sm  carouselDiv" id="img1">
           <div className="eventTextDiv container-xs" >
             <h1 className={"eventTitle text-center" } ref = {this.eventTitle1}>Meltdown</h1>
-            <p className="text-white text-center eventDesc" ref = {this.eventDesc1}>fcgvhugyftvjkhugyfcghvjhbkj</p>
+            <p className="text-white text-center eventDesc" ref = {this.eventDesc1}>Where all the metal heads go Crazy</p>
           </div>
           </div>
           <div className="container-sm carouselDiv" id="img2">
           <div className="eventTextDiv container-xs">
             <h1 className=" eventTitle text-center" ref={this.eventTitle2}>Euphonic</h1>
-            <p className="text-white text-center eventDesc1" ref={this.eventDesc2}>fcgvhugyftvjkhugyfcghvjhbkj</p>
+            <p className="text-white text-center eventDesc" ref={this.eventDesc2}>The dopest introduction to music club</p>
           </div>
           </div>
           <div className="container-sm carouselDiv" id="img3">
           <div className="eventTextDiv container-xs">
-            <h1 className=" eventTitle text-center" ref={this.eventTitle3}>Meltdown</h1>
-            <p className="text-white text-center eventDesc1" ref={this.eventDesc3}>fcgvhugyftvjkhugyfcghvjhbkj</p>
+            <h1 className=" eventTitle text-center" ref={this.eventTitle3}>Unplugged</h1>
+            <p className="text-white text-center eventDesc" ref={this.eventDesc3}>Just raw beautiful talent , no wires attached</p>
           </div>
           </div>
 
           <div className="container-sm carouselDiv" id="img4">
           <div className="eventTextDiv container-xs">
-            <h1 className=" eventTitle text-center" ref={this.eventTitle4}>Meltdown</h1>
-            <p className="text-white text-center eventDesc1" ref={this.eventDesc4}>fcgvhugyftvjkhugyfcghvjhbkj</p>
+            <h1 className=" eventTitle text-center" ref={this.eventTitle4}>Roadblock</h1>
+            <p className="text-white text-center eventDesc" ref={this.eventDesc4}>Bring the music to the streets</p>
           </div>
           </div>
         </AwesomeSlider>
