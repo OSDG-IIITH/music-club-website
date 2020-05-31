3f637b3cb73a47785e017d26e79a7042a4f9b1d5
Mehul1604 <mathurmehul3@gmail.com>
2020-05-31 14:39:39 +0530

    made more responsive made buttons smaller and added opacity test effect

--- music_club_website_base_template/src/components/Home/Home.js
+++ music_club_website_base_template/src/components/Home/Home.js
@@ -14,15 +14,40 @@ import 'popper.js';
 import '../Home/Home.css';
 
 class Home extends Component {
+
+  state = {
+    isOnDisplay : true
+  }
+
+  // fade = () =>{
+    
+  //   this.setState({
+  //     isOnDisplay:true
+  //   })
+  //   console.log("the display state changed to " + this.state.isOnDisplay +  " on transition start");
+  // }
+
+  // bringBack = () =>{
+  //   this.setState({
+  //     isOnDisplay:false
+  //   })
+
+  //   console.log("the display state changed to " + this.state.isOnDisplay +  " on transition end");
+  // }
+
+   eventTitle  = React.createRef();
+   eventDesc = React.createRef();
+ 
   render() {
+    console.log(this.state.isOnDisplay)
     return (
       <React.Fragment>
       <div className="container-fluid mainDiv bg-dark">
-        <AwesomeSlider className="carousel container-sm" cssModule={styles}>
+        <AwesomeSlider className="carousel container-sm" cssModule={styles} transitionDelay={500} mobileTouch={true} bullets={true} onTransitionStart={()=> {this.eventTitle.current.style.opacity='40%' ; this.eventDesc.current.style.opacity='40%'} } onTransitionEnd={()=> {this.eventTitle.current.style.opacity='100%' ; this.eventDesc.current.style.opacity='100%'}}  >
           <div className=" container-sm  carouselDiv" id="img1">
           <div className="eventTextDiv container-xs">
-            <h1 className=" eventTitle text-center">Meltdown</h1>
-            <p className="text-white text-center eventDesc">fcgvhugyftvjkhugyfcghvjhbkj</p>
+            <h1 className={"eventTitle text-center" } ref = {this.eventTitle}>Meltdown</h1>
+            <p className="text-white text-center eventDesc" ref = {this.eventDesc}>fcgvhugyftvjkhugyfcghvjhbkj</p>
           </div>
           </div>
           <div className="container-sm carouselDiv" id="img2">
