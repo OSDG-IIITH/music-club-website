e7d066483a96bfff6ab02acc964205ffa95913e1
Prajneya <prajneya.kumar@research.iiit.ac.in>
2020-06-03 19:17:43 +0530

    Add Events Template FrontEnd

---
+++ music_club_website_base_template/src/components/Gallery/Mixed/gallery.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+//@ts-ignore
+import 'bootstrap/dist/js/bootstrap';
+// to import react-bootstrap import {...} from 'react-bootstrap'
+import 'jquery';
+import 'popper.js';
+
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="row">
+      Hi.
+      </div>
+    )
+  }
+}
+
+export default Home;
