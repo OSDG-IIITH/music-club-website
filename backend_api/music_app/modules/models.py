0bfe7469a60b113bbc8057a2e424588f8d169abd
Archit Jain <54016441+Architjain128@users.noreply.github.com>
2020-06-21 22:34:42 +0530

    datatypes are changed
    event_id , date and time

--- backend_api/music_app/modules/models.py
+++ backend_api/music_app/modules/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String , Date ,Time
 from modules.database import Base
 from sqlalchemy.orm import relationship
 
@@ -6,13 +6,13 @@ from sqlalchemy.orm import relationship
 class Event(Base):
     __tablename__ = "event"
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(String, primary_key=True, index=True)
     name = Column(String,index=True)
     state = Column(String,index=True)
     poster = Column(String,index=True)
     description = Column(String,index=True)
-    date = Column(String,index=True)
-    time = Column(String,index=True)
+    date = Column(Date,index=True)
+    time = Column(Time,index=True)
     venue = Column(String,index=True)
     gallery_link = Column(String,index=True)
     ping_link = Column(String,index=True)
@@ -23,7 +23,7 @@ class Registration(Base):
     __tablename__ = "registration"
 
     id = Column(Integer, primary_key=True, index=True)
-    event_id = Column(Integer,ForeignKey("event.id"))
+    event_id = Column(String,ForeignKey("event.id"))
     band_name = Column(String,index=True)
     player_names = Column(String,index=True)
     instrument_names = Column(String,index=True)
@@ -37,7 +37,7 @@ class Lineup(Base):
     __tablename__ = "lineup"
 
     id = Column(Integer, primary_key=True, index=True)
-    event_id = Column(Integer,ForeignKey("event.id"))
+    event_id = Column(String,ForeignKey("event.id"))
     band_name = Column(String,index=True)
     slot_given = Column(String,index=True)
     slot_number = Column(String,index=True)
@@ -47,7 +47,8 @@ class Photos(Base):
     __tablename__ = "photos"
 
     id = Column(Integer, primary_key=True, index=True)
-    event_id = Column(Integer,ForeignKey("event.id"))
+    event_id = Column(String,ForeignKey("event.id"))
+    photo_id = Column(String,primary_key=True,index=True)
     label = Column(String,index=True)
     link = Column(String,index=True)
 
