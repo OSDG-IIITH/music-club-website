6659f3e4e34105c6976c207d89422ed284985e9f
Archit Jain <54016441+Architjain128@users.noreply.github.com>
2020-06-21 23:02:47 +0530

    add items of adminuser.py

--- backend_api/music_app/routers/admin.py
+++ backend_api/music_app/routers/admin.py
@@ -1,12 +1,17 @@
-from fastapi import APIRouter, Query, Body, Path, Header, Depends
+from fastapi import APIRouter, Query, Body, Path, Header, Depends, HTTPException, status
 from fastapi.encoders import jsonable_encoder
+from typing import List, Dictfrom fastapi.security
 from typing import List, Dict
 from modules import schemas
 from modules import models
+import jwt
+from jwt import PyJWTError
+from passlib.context import CryptContext
 from sqlalchemy.orm import Session
 from modules.database import SessionLocal
 router = APIRouter()
 
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def get_db():
     db = SessionLocal()
@@ -26,6 +31,34 @@ def get_db():
 #     db.commit()
 #     db.refresh(db_registered)
 #     return "photo has been added to db!"
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+@router.post('/changepassword')
+async def change_password( db: Session = Depends(get_db), user : schemas.AdminPassword):
+    if user.password == user.confirmpassword :
+        new_username = user.username['username']
+        new_username = get_password_hash(user.password)['password']
+        db_user = db.query(models.User).filter(models.User.id == 1 )
+        db_user.update({models.User : new_username , new_password})
+        db.commit()
+        return "username and password is updated"
+    return "confirm password did't match"
+
+@router.post('/login', response_model=List[schemas.AdminDetail])
+async def get_password(db: Session = Depends(get_db)):
+    # from /login form
+    name=user.username
+    paas=user.password 
+    # from database table named User
+    db_ok = db.query(models.User).filter(models.User.id == 1 )
+    if db_ok.username = name && verify_password(paas, db_ok.password) :
+        return true  # value neede to be used in login.js
+    return false
+
 
 @router.post('/addEvent')
 async def add_event(newEvent : schemas.EventCreate = Body(...) , db : Session = Depends(get_db)):
