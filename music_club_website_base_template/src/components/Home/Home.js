d6e2d32383556dcc020df69875b6cd205c353555
Mehul1604 <mathurmehul3@gmail.com>
2020-06-22 20:58:56 +0530

    design changes

--- music_club_website_base_template/src/components/Home/Home.js
+++ music_club_website_base_template/src/components/Home/Home.js
@@ -102,7 +102,7 @@ class Home extends Component {
     if (this.eventTitle1.current) {
       this.eventTitle1.current.style.opacity = '20%';
       this.eventDesc1.current.style.opacity = '20%';
-      this.eventTitle1.current.style.transform = 'translate(0px , -700px)';
+      this.eventTitle1.current.style.transform = 'translate(0px , -900px)';
       this.eventDesc1.current.style.transform = 'translate(-1100px , 0px)';
       this.regBtn1.current.style.transform = 'translate(-1100px, 0px)';
       this.linBtn1.current.style.transform = 'translate(-1100px, 0px)';
@@ -111,21 +111,21 @@ class Home extends Component {
     if (this.eventTitle2.current) {
       this.eventTitle2.current.style.opacity = '20%';
       this.eventDesc2.current.style.opacity = '20%';
-      this.eventTitle2.current.style.transform = 'translate(0px , -700px)';
+      this.eventTitle2.current.style.transform = 'translate(0px , -900px)';
       this.eventDesc2.current.style.transform = 'translate(-1100px , 0px)';
     }
 
     if (this.eventTitle3.current) {
       this.eventTitle3.current.style.opacity = '20%';
       this.eventDesc3.current.style.opacity = '20%';
-      this.eventTitle3.current.style.transform = 'translate(0px , -700px)';
+      this.eventTitle3.current.style.transform = 'translate(0px , -900px)';
       this.eventDesc3.current.style.transform = 'translate(-1100px , 0px)';
     }
 
     if (this.eventTitle4.current) {
       this.eventTitle4.current.style.opacity = '20%';
       this.eventDesc4.current.style.opacity = '20%';
-      this.eventTitle4.current.style.transform = 'translate(0px , -700px)';
+      this.eventTitle4.current.style.transform = 'translate(0px , -900px)';
       this.eventDesc4.current.style.transform = 'translate(-1100px , 0px)';
     }
 
@@ -284,7 +284,7 @@ class Home extends Component {
               </div>
             </div>
             <div className="carouselDiv" id="img2">
-              <div className="container-xs">
+              <div className="eventTextDiv">
                 <h1 className=" eventTitle text-center" ref={this.eventTitle2}>Euphonic</h1>
                 <p className="text-white text-center eventDesc" ref={this.eventDesc2}>The dopest introduction to music club</p>
               </div>
@@ -421,9 +421,7 @@ class Home extends Component {
           </div>
           {/* <div className='jumbotron landingPageFooter'>
             <div className='text-center footer'> */}
-              <span id='footer-note text-center'>
-              &copy; Copyright: lorem-ipsum@gmail.com
-              </span>
+              
             {/* </div>
           </div> */}
       </React.Fragment>
