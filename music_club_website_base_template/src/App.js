9b937e22571f11330ab540d64bc3e70b2a2789d3
Archit Jain <54016441+Architjain128@users.noreply.github.com>
2020-06-28 02:11:09 +0530

    Update App.js

--- music_club_website_base_template/src/App.js
+++ music_club_website_base_template/src/App.js
@@ -1,14 +1,14 @@
-import React,{Component} from 'react';	
-import { BrowserRouter, Route, Switch} from 'react-router-dom';	
-import SimpleReactLightbox from 'simple-react-lightbox'	
+import React,{Component} from 'react';
+import { BrowserRouter, Route, Switch} from 'react-router-dom';
+import SimpleReactLightbox from 'simple-react-lightbox'
 
-// import './App.css';	
-import 'bootstrap/dist/css/bootstrap.css';	
-//@ts-ignore	
-import 'bootstrap/dist/js/bootstrap';	
-// to import react-bootstrap import {...} from 'react-bootstrap'	
-import 'jquery';	
-import 'popper.js';	
+// import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+//@ts-ignore
+import 'bootstrap/dist/js/bootstrap';
+// to import react-bootstrap import {...} from 'react-bootstrap'
+import 'jquery';
+import 'popper.js';
 
 import Home from './components/Home/Home';
  import Timeline from './components/Timeline/Timeline';
@@ -18,6 +18,8 @@ import Event from './components/Gallery/Events/event';
  import Admin from './components/login/Admin';
  import Logout from './components/login/Logout';
 import CreateEvent from './components/admin/create_event';
+import UpdateEvent from './components/admin/update_event';
+import DeleteEvent from './components/admin/delete_event';
 
 class App extends Component {
   render(){
@@ -36,6 +38,8 @@ class App extends Component {
               <Route path="/logout" component={Logout} exact/>  
               <Route path="/admin" component={Admin} exact/>   
               <Route path="/admin/create_event" component={CreateEvent} exact/>   
+              <Route path="/admin/change_event" component={UpdateEvent} exact/>   
+              <Route path="/admin/delete_event" component={DeleteEvent} exact/>   
             </Switch>
           </div>
         </BrowserRouter>
