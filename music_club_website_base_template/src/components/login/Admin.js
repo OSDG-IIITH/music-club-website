5e02673a6f1dfaba39db5a03e00da229d84615e6
Archit Jain <54016441+Architjain128@users.noreply.github.com>
2020-06-28 00:03:53 +0530

    Update Admin.js

--- music_club_website_base_template/src/components/login/Admin.js
+++ music_club_website_base_template/src/components/login/Admin.js
@@ -24,8 +24,43 @@ export default class Admin extends Component {
         }
         return (
             <div id="log">
-                <h2 id="admin-topline">Welcome to admin page</h2>
-                <Link id="admin-logout-button" to="/logout">logout</Link>
+            <div id="ok">
+
+                    <h3 id="admin-topline">Welcome to admin page</h3>
+                <div id="pun">
+                    <div id="content-box-1">
+                        <h3>Events</h3>
+                    </div>
+                    <div id="content-box-2">
+                        <Link id="create-event" to="/admin/create_event">Create an event</Link><br/>
+                        <Link id="update-event-state" to="/admin/change_state">Update event state</Link><br/>
+                        <Link id="delete-event" to="/admin/delete_event">Delete an event</Link><br/>
+                    </div>
+                </div>
+                <div id="pun">
+                    <div id="content-box-1">
+                        <h3>Photos</h3>
+                    </div>
+                    <div id="content-box-2">
+                        <Link id="add-photo" to="/admin/add_photo">Add an image to database </Link><br/>
+                        <Link id="delete-photo" to="/admin/delete_photo">Remove an image from database</Link><br/>
+                    </div>  
+                </div>
+                <div id="pun">
+                    <div id="content-box-1">
+                        <h3>Lineup</h3>
+                    </div>
+                    <div id="content-box-2">
+                        <Link id="add-lineup" to="/admin/add_lineup">Add a lineup</Link><br/>
+                    </div> 
+                </div>
+                <div></div>
+                <div id="punk">
+                    
+                    <Link id="admin-button"  to="/logout">Logout from admin</Link><br/>
+                    <Link id="admin-button" to="/admin/create_event">Change Password</Link><br/>
+                </div>
+                </div>
             </div>
         )
     }
