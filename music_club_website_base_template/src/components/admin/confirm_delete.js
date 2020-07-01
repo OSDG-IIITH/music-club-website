03bd6883329df1fa1abd622fb5146b196690c230
Archit Jain <54016441+Architjain128@users.noreply.github.com>
2020-07-01 19:05:12 +0530

    Create confirm_delete.js

---
+++ music_club_website_base_template/src/components/admin/confirm_delete.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react'
+import {Link,Redirect} from 'react-router-dom'
+ import './admin.css'
+  
+
+ export default class ConfirmDelete extends Component {
+    state = {
+            photo_id:'',
+            loggedIn : true,
+            access_token : null
+        }
+
+    render() {
+        if(this.state.loggedIn === false)
+        {
+            return <Redirect to="/login" />
+        }
+        return (
+                <div class="ok">
+                        <div class="container">
+                        <h6 id="mes">*Press confirm to delete</h6>
+                            <form >
+
+                                <div class="pre">
+                                   <div>
+                                        <img id="prep" src={require('./No-Image-Found.png')}></img>
+                                   </div>
+                                   <div id="dis">
+                                        <div class="row">
+                                            <div class="col-25">
+                                                <label id="label2" for="name">Event Name:</label>
+                                            </div>
+                                            <div class="col-75">
+                                                <label id="label3" name="name" > (event name from db)</label>                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-25">
+                                                <label id="label2" for="name">Image Label:</label>
+                                            </div>
+                                            <div class="col-75">
+                                                <label id="label3" name="name" > (label from db)</label>                                            </div>
+                                        </div>
+                                   </div>
+                                </div>                            
+
+                                <div class="row">
+                                    <input type="submit" id="create" value="Confirm"></input>
+                                </div>
+
+                            </form>
+                        </div>
+                </div>      
+            )
+    }
+    
+}
